7d1b75bdd0763c59dcbdec0578c43bd59ec38fb3
Gosia <lm.sprachen@gmail.com>
2024-07-22 23:54:14 +0200

    add fruitlist_component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
+import { FruitlistComponent } from './fruitlist/fruitlist.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, FruitlistComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
